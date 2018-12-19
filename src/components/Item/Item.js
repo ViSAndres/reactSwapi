6d268f81c5c736136187a60e3549bbf0d58a6b80
CarlosMac <carlosneira1997@gmail.com>
2018-12-19 00:35:42 -0500

    Created initial hovering function

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -3,18 +3,31 @@ import './Item.css';
 class Item extends Component {
     constructor(props){
         super(props);
+        this.handleMouseHover = this.handleMouseHover.bind(this);
         this.state = {
+            isHovering: false,
             Data: this.props.itemData,
         }
     }
+
+    handleMouseHover() {
+        this.setState(this.toggleMouseHover)
+    }
+
+    toggleMouseHover(state) {
+        return {
+            isHovering: !state.isHovering,
+        };
+    }
+
 render(){
     return(
         <div className = 'Box'>
-        <p1>{this.props.itemData.name}</p1>
+        <div onMouseEnter = {this.handleMouseHover} onMouseLeave = {this.handleMouseHover} >{this.props.itemData.name}</div>
+        {this.state.isHovering && <div className = 'Hover'> {this.state.Data.birth_year} </div>}
         </div>
     )
 }
 }
-
 
 export default Item;
